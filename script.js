1f67a6300e955cb1ed14c0540ad27331ca6467d2
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-05-19 20:37:42 -0300

    commit atualização e inicio de script

---
+++ script.js
@@ -0,0 +1,9 @@
+var n = document.getElementById('num')
+var num = Number(n.value)
+let lista = document.getElementById('lista')
+let msg = document.getElementById('msg')
+let valores = []
+
+function adcionar() {
+
+}
