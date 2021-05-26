f25ad684e0079550c68734d9603cf780cb821e6d
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-05-26 14:30:13 -0300

    up

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-var n = document.getElementById('num')
-var num = Number(n.value)
-let lista = document.getElementById('lista')
-let msg = document.getElementById('msg')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let msg = document.querySelector('div#res')
 let valores = []
 
-function adcionar() {
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
 
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adcionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        document.alert('tudo ok')
+    }else {
+        document.alert('Valor invalido ou já encontrado.')
+    }
 }
